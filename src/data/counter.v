d8970fa6e6627d3386732a28abad888b90cd6b4e
shiipou <shiishii@nocturlab.fr>
2022-10-01 12:02:38 +0200

    fix: don't use optionnal in struct because it's unstable for now

--- src/data/counter.v
+++ src/data/counter.v
@@ -2,7 +2,13 @@ module data
 
 pub struct Counter {
 pub:
-	id    ?string [json: _id]
+	count u32
+	user  string
+}
+
+pub struct SavedCounter {
+pub:
+	id    string [json: _id]
 	count u32
 	user  string
 }
--- src/listeners/increment.v
+++ src/listeners/increment.v
@@ -1,14 +1,14 @@
 module listeners
 
 import x.json2 { Any }
-import src.data { Api, Counter }
+import src.data { Api, SavedCounter }
 
 pub fn increment(props map[string]Any, event map[string]Any, api Api) ? {
 	eprintln('Listener{name: \'increment\', props: $props, event: $event, api: $api}')
-	counter := api.get_document<Counter>(data.counter_collection, props['id']?.str()) or {
+	counter := api.get_document<SavedCounter>(data.counter_collection, props['id']?.str()) or {
 		panic(err)
 	}
-	api.update_document<Counter>(data.counter_collection, Counter{
+	api.update_document<SavedCounter>(data.counter_collection, SavedCounter{
 		...counter
 		count: counter.count + 1
 	}) or { panic(err) }
